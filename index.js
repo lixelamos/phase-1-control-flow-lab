3402ad704ba92e077e6ffe49d8c89527f8558b85
Lixel Amos <amosbett99@gmail.com>
2023-03-14 11:44:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  if (distance>2500)
+  return "No can do."
   // Write your code here!
+  if (distance<= 400){
+    return('This one is on me!')
+  }
+  else  (distance > 2001) 
+    return ('I will gladly take your thirty bucks.');
+  
+
+if(distance >2500){
+return ('No can do.');
+}
+  
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+return city=="NYC"?"Ok, sounds good."
+:
+"No go.";
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip)
+{
+  case "generous":
+  return "Thank you so much."
+  break;
+  case "not as generous":
+  return "Thank you.";
+  break;
+ default : 
+return "Bye.";
 }
+}
+
